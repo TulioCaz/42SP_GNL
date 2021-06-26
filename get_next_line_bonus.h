55433dc16811561955146ab1c682d2c907132e82
TulioCaz <tuliocaz.windcoast@gmail.com>
2021-06-26 17:24:03 -0300

    Fix 42header

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tuliokaaz <tuliokaaz@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/23 16:56:03 by tuliokaaz         #+#    #+#             */
-/*   Updated: 2021/06/25 20:13:12 by tuliokaaz        ###   ########.fr       */
+/*   Created: 2021/06/26 17:21:45 by tuliokaaz         #+#    #+#             */
+/*   Updated: 2021/06/26 17:22:51 by tuliokaaz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
